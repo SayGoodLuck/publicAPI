2d48005965eb61f7bfa56a91200b98d57102dc69
saygoodluck <saaygoodluck@gmail.com>
2023-01-23 02:32:13 +0100

    fixxx

--- src/main/java/com/example/springboot/student/StudentConfig.java
+++ src/main/java/com/example/springboot/student/StudentConfig.java
@@ -1,2 +1,25 @@
-package com.example.springboot.student;public class StudentConfig {
+package com.example.springboot.student;
+
+import com.example.springboot.student.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student mike = new Student(
+                    "Mike Tyson",
+                    LocalDate.of(1966, Month.JUNE, 30),
+                    "mike.tyson@gmail.com");
+            studentRepository.save(mike);
+        };
+
+    }
 }
--- src/main/java/com/example/springboot/student/controller/StudentController.java
+++ src/main/java/com/example/springboot/student/controller/StudentController.java
@@ -1,8 +1,43 @@
-package com.example.springboot.student;
+package com.example.springboot.student.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springboot.student.Student;
+import com.example.springboot.student.service.StudentService;
+import jakarta.persistence.PostUpdate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student) {
+        studentService.create(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long id) {
+        studentService.deleteStudent(id);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long id,
+                              @RequestParam(required = false) String email,
+                              @RequestParam(required = false) String name){
+        studentService.updateStudent(id, name, email);
+    }
 }
